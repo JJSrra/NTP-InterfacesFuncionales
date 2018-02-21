bf64edb02cf72951d4c2e43f53d6ab2f889db05d
Juanjo Sierra <jjsierra103@gmail.com>
2018-02-21 11:26:04 +0100

    Utilizando flujo IntStream para mostrar los elementos de una colección

---
+++ src/OperacionesIntStream.java
@@ -0,0 +1,47 @@
+import java.util.Random;
+import java.util.function.*;
+import java.util.stream.IntStream;
+
+public class OperacionesIntStream {
+    private int valores[];
+
+    public OperacionesIntStream(int numeroValores){
+        valores = new int[numeroValores];
+        Random generador = new Random();
+        for(int i=0; i < numeroValores; ++i){
+            valores[i] = generador.nextInt(101);
+        }
+    }
+
+    // Método para mostrar los valores (paradigma imperativo)
+    public void mostrarValoresImperativo(){
+        for(int i=0; i < valores.length; ++i){
+            System.out.printf("%d", valores[i]);
+        }
+        System.out.println();
+    }
+
+    // Método para mostrar valores (paradigma funcional)
+    public void mostrarValoresFuncional(){
+        // Definir el flujo, y con el método "of" se indica sobre qué colección se genera
+        IntStream flujo = IntStream.of(valores);
+
+        // Definir la operación a realizar
+        IntConsumer operacion = valor -> System.out.printf("%d", valor);
+
+        // Se realiza la operación
+        flujo.forEach(operacion);
+
+        System.out.println();
+    }
+
+    // Otra forma más directa de definir la función anterior según el paradigma funcional
+    public void mostrarValoresFuncional2(){
+        IntStream.of(valores).forEach(valor -> System.out.printf("%d", valor));
+        System.out.println();
+    }
+
+    public static void main(String args[]){
+        OperacionesIntStream objetos = new OperacionesIntStream(1000);
+    }
+}
